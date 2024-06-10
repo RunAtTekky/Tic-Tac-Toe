149b1c7ec4bcc8601b8b53204f90341584a1bdb8
Varun <varunrawat343@gmail.com>
2024-06-10 12:56:48 +0530

    Move onClick function to the div containing the item and add cursor: pointer

--- src/component/Grid.js
+++ src/component/Grid.js
@@ -4,7 +4,7 @@ const Grid = () => {
   const [p1_turn, setP1_turn] = useState(true);
   const [marks, setMarks] = useState(Array(9).fill("minus"));
   const [gameOver, setGameOver] = useState(false);
-  const [col, setCol] = useState("black")
+  const [col, setCol] = useState("black");
 
   const handlePlay = useCallback(
     (index) => {
@@ -24,23 +24,39 @@ const Grid = () => {
   const handleRestart = () => {
     setMarks(Array(9).fill("minus"));
     setGameOver(false);
-  }
+  };
 
   const hasWon = useCallback(() => {
-    for (let i=0; i<3; i++) {
-      if (marks[i] !== "minus" && marks[i] === marks[i+3] && marks[i] === marks[i+6]) {
+    for (let i = 0; i < 3; i++) {
+      if (
+        marks[i] !== "minus" &&
+        marks[i] === marks[i + 3] &&
+        marks[i] === marks[i + 6]
+      ) {
         setGameOver(true);
       }
     }
-    for (let i=0; i<=6; i++) {
-      if (marks[i] !== "minus" && marks[i] === marks[i+1] && marks[i] === marks[i+2]) {
+    for (let i = 0; i <= 6; i++) {
+      if (
+        marks[i] !== "minus" &&
+        marks[i] === marks[i + 1] &&
+        marks[i] === marks[i + 2]
+      ) {
         setGameOver(true);
       }
     }
-    if (marks[0] !== "minus" && marks[0] === marks[4] && marks[4] === marks[8]) {
+    if (
+      marks[0] !== "minus" &&
+      marks[0] === marks[4] &&
+      marks[4] === marks[8]
+    ) {
       setGameOver(true);
     }
-    if (marks[2] !== "minus" && marks[2] === marks[4] && marks[4] === marks[6]) {
+    if (
+      marks[2] !== "minus" &&
+      marks[2] === marks[4] &&
+      marks[4] === marks[6]
+    ) {
       setGameOver(true);
     }
   }, [marks]);
@@ -52,26 +68,39 @@ const Grid = () => {
   if (gameOver) {
     return (
       <div className="cont">
-        <h1 style={{color: `${col === "green" ? "red" : "green"}`}}>{p1_turn === true ? "Player 2 with O Won" : "Player 1 with X won"} </h1>
-        <button className="btn" onClick={handleRestart}>Restart</button>
+        <h1 style={{ color: `${col === "green" ? "red" : "green"}` }}>
+          {p1_turn === true ? "Player 2 with O Won" : "Player 1 with X won"}{" "}
+        </h1>
+        <button className="btn" onClick={handleRestart}>
+          Restart
+        </button>
       </div>
     );
   }
   return (
     <div className="cont">
-        <h1>Tic Tac Toe</h1>
+      <h1>Tic Tac Toe</h1>
       <div className="grid-container">
         {marks.map((mark, index) => (
-          <div key={index} className="grid-item">
+          <div
+            onClick={() => handlePlay(index)}
+            key={index}
+            className="grid-item"
+          >
             <i
               className={`fa-solid fa-${mark}`}
-              onClick={() => handlePlay(index)}
-              style={{color: `${mark === "minus" ? col : (mark === "xmark" ? "red" : "green")}`}}
+              style={{
+                color: `${
+                  mark === "minus" ? col : mark === "xmark" ? "red" : "green"
+                }`,
+              }}
             ></i>
           </div>
         ))}
       </div>
-      <button className="btn" onClick={handleRestart}>Restart</button>
+      <button className="btn" onClick={handleRestart}>
+        Restart
+      </button>
     </div>
   );
 };
